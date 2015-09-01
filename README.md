f70ad160f405176b1df52116a8a5162a68f7c96c
Baptiste Morelle <bapmrl@gmail.com>
2015-09-01 10:07:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# bapmrl-react-multiselect
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Multiselect extends Component {
+  render() {
+    return (
+      <p>Hello, world!</p>
+    );
+  }
+}
